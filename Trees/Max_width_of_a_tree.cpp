c0f1b2e67006f3a5b8a241e0ab7f2d23395ef0ba
abhijnanacharya <abhijnanacharya11@gmail.com>
2022-08-21 19:49:02 +0530

    Max_width of a tree

---
+++ Trees/Max_width_of_a_tree.cpp
@@ -0,0 +1,174 @@
+/* PROBLEM STATEMENT:Maximum Width of Tree
+Given a Binary Tree, find the maximum width of it. Maximum width is defined as the maximum number of nodes in any level.
+For example, maximum width of following tree is 4 as there are 4 nodes at 3rd level.
+          1
+       /     \
+     2        3
+   /  \      /  \
+  4    5    6    7
+   \
+    8
+
+
+Example 1:
+
+Input:
+       1
+     /    \
+    2      3
+Output: 2
+Example 2:
+
+Input:
+        10
+      /     \
+    20      30
+   /    \
+  40    60
+Output: 2
+
+*/
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// Tree Node
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+
+// Utility function to create a new Tree Node
+Node* newNode(int val) {
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+
+// Function to Build Tree
+Node* buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N') return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;) ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size()) break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+// } Driver Code Ends
+/*  Structure of a Binary Tree
+
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+ */
+
+class Solution {
+  public:
+    // Function to get the maximum width of a binary tree.
+    int getMaxWidth(Node* root) {
+
+        // Your code here
+        //APPROACH: SIMPLE BFS(LEVEL ORDER TRAVERSAL) Travel each level of the tree and find the max Width.
+        if(!root) return 0;
+        queue<Node*>q;
+        q.push(root);
+        int width=1;
+        while(!q.empty()){
+            int size=q.size();
+            width=max(width,size); //In each iteration q.size() represents the number of the children in the current level
+            while(size--){
+               Node* temp=q.front();
+               q.pop();
+                if(temp->left)
+                    q.push(temp->left);
+                if(temp->right)
+                    q.push(temp->right);
+           }
+        }
+        return width;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    getchar();
+    while (t--) {
+        string inp;
+        getline(cin, inp);
+        Node* root = buildTree(inp);
+
+        Solution ob;
+        cout << ob.getMaxWidth(root) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
