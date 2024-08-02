d45dabaf79064ec34d9e7467bac58e76ea39e7c9
Abhijnan Acharya <abhijnanacharya11@gmail.com>
2024-08-02 15:56:09 -0500

    Added MOD Arithmetic

--- test_drive/test.cpp
+++ test_drive/test.cpp
@@ -1,11 +1,53 @@
-#include<bits/stdc++.h>
+/*
+	FIBONACCI + MODULAR ARITHMETIC+ MODULO INVERSE USING FERMATS LITTLE THEOREM
+
+*/
+#include <bits/stdc++.h>
+#define MOD 1000000007
+#define int long long
 using namespace std;
 
-int main(){
-	
-	int n=10;
 
-	cout<< n;
+int fact[100005];
+int power(int base, int n){
+    int ans=1;
+    while(n){
+        if(n%2){
+            n=n-1;
+            ans=(ans*base)%MOD;
+        }else{
+            n=n/2;
+            base=(base*base)%MOD;
+        }
+    }
+    return ans;
+}
+void precomputeFact(){
+    fact[0]=1;
+    fact[1]=1;
+    
+    for(int i=2;i<100005;i++){
+        fact[i]=(i%MOD * fact[i-1]%MOD)%MOD;
+    }
+}
+int nCr(int n, int r){
+     
+		return  (fact[n]%MOD*(power(fact[r],MOD-2)%MOD * power(fact[n-r],MOD-2)%MOD)%MOD)%MOD;
+}
 
-	return 0;
+
+
+
+signed main()
+{
+    int T;
+    cin>>T;
+    precomputeFact();
+    while(T--){
+        int n,r;
+        cin>>n>>r;
+        cout<<nCr(n,r)<<endl;
+    }
+
+    return 0;
 }
